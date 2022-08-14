3813a9d6b26c1d100551ce5e3a4b9e3d75b21820
Luciano Rodrigues <a_luciano10@hotmail.com>
2022-08-14 18:29:07 -0300

    Primeiro commit com Vue

---
+++ project_school_init/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  <AlunosFacul/>
+  </div>
+</template>
+
+<script>
+import AlunosFacul from './components/Aluno/AlunosFacul.vue'
+
+export default {
+  name: 'app',
+  components: {
+    AlunosFacul
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
